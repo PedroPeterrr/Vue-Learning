27b3de3389c9cf7943a62aaae5b2fe0ef9525f19
asd <cabantog.peterjames@gmail.com>
2025-11-19 12:15:39 +0800

    Restructure project and add Vue 3 Vite template
    Moved existing HTML and JS files into the '1st' directory for organization. Added a new 'vue-project' directory with a Vue 3 + Vite starter template, including configuration files, recommended extensions, and initial source files.

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useStorage } from '@/composables/useStorage';
+
+let anime = useStorage('anime')
+let movie = useStorage('movie')
+</script>
+
+<template>
+  <main>
+    <p>
+      What is your favorite Anime? <input type="text" v-model="anime">
+    </p>
+
+     <p>
+      What is your favorite Movie? <input type="text" v-model="movie">
+    </p>
+  </main>
+</template>
